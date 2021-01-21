88f35e5894515505bbec8208c26074e5e467e6b1
Lucas Draescher <draescherl@eisti.eu>
2021-01-21 22:44:38 +0100

    Quick code reformat

--- index.js
+++ index.js
@@ -2,21 +2,22 @@
 const discord = require('discord.js');
 
 /* Fetch creds */
-const { token } = require("./creds.json");
+const { token } = require('./creds.json');
 
 /* Bot init */
 const client = new discord.Client;
 client.once('ready', () => {
-  console.log("AIR-BOT is online.");
+  console.log('AIR-BOT is online.');
   client.user.setActivity('RODR PD', { type: 'PLAYING' });
 });
 
 
 /* Temporary voice channel */
 client.on('voiceStateUpdate', (oldMember, newMember) => {
+  
   /* Delete channel once everybody has left */
   if (oldMember.channel != undefined) {
-    if (oldMember.channel.name.startsWith("tmp")) {
+    if (oldMember.channel.name.startsWith('tmp')) {
       if (oldMember.channel.members.size == 0) {
         oldMember.channel.delete();
       }
@@ -24,15 +25,18 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
   }
 
   /* Create new channel once someone clicks on the trigger */
-  if ((newMember.channel != undefined) && (newMember.channel.name === "Créer salon vocal")) {
-    let channelName = "tmp" //+ newMember.channel.parentId.children.size - 1;
-    newMember.guild.channels.create(channelName, {
-      type: 'voice',
-      parent: newMember.channel.parentID
-    }).then(vc => {
-      newMember.setChannel(vc);
-    })
+  if (newMember.channel != undefined) {
+    if (newMember.channel.name === 'Créer salon vocal') {
+      let channelName = 'tmp' //+ newMember.channel.parentId.children.size - 1;
+      newMember.guild.channels.create(channelName, {
+        type: 'voice',
+        parent: newMember.channel.parentID
+      }).then(vc => {
+        newMember.setChannel(vc);
+      })
+    }
   }
+
 });
 
 
