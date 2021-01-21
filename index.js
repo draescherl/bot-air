099b0174cffdf3e09e098befb8742aad871e00a9
Lucas Draescher <draescherl@eisti.eu>
2021-01-21 22:41:00 +0100

    Got channel creation/deletion working

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 /* Imports */
 const discord = require('discord.js');
-const fs = require('fs');
 
+/* Fetch creds */
+const { token } = require("./creds.json");
+
+/* Bot init */
 const client = new discord.Client;
-client.commands = new discord.Collection();
-
-/* Log on bot start */
 client.once('ready', () => {
   console.log("AIR-BOT is online.");
   client.user.setActivity('RODR PD', { type: 'PLAYING' });
 });
 
-/* Fetch creds */
-var contents = fs.readFileSync("creds.JSON");
-var creds = JSON.parse(contents);
+
+/* Temporary voice channel */
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+  /* Delete channel once everybody has left */
+  if (oldMember.channel != undefined) {
+    if (oldMember.channel.name.startsWith("tmp")) {
+      if (oldMember.channel.members.size == 0) {
+        oldMember.channel.delete();
+      }
+    }
+  }
+
+  /* Create new channel once someone clicks on the trigger */
+  if ((newMember.channel != undefined) && (newMember.channel.name === "Créer salon vocal")) {
+    let channelName = "tmp" //+ newMember.channel.parentId.children.size - 1;
+    newMember.guild.channels.create(channelName, {
+      type: 'voice',
+      parent: newMember.channel.parentID
+    }).then(vc => {
+      newMember.setChannel(vc);
+    })
+  }
+});
+
 
 /* Run bot */
-client.login(creds.token);
+client.login(token);
